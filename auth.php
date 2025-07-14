172524008fca373d7268c0e1fc4a6834ac1e2697
Saichul Huda <saichulhuda1995@gmail.com>
2025-07-14 10:00:47 +0700

    fix auth & add endpoint

--- auth.php
+++ auth.php
@@ -1,4 +1,7 @@
 <?php
+require_once "config.php";
+require_once "function.php";
+
 if (empty($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_PW'])) {
     header('WWW-Authenticate: Basic realm="Restricted Area"');
     header('HTTP/1.0 401 Unauthorized');
@@ -10,12 +13,9 @@ if (empty($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_PW'])) {
 $username = $_SERVER['PHP_AUTH_USER'];
 $password = $_SERVER['PHP_AUTH_PW'];
 
-if ($username != 'test') {
-    header('WWW-Authenticate: Basic realm="Restricted Area"');
-    header('HTTP/1.0 401 Unauthorized');
-    echo json_encode(["status" => "error", "message" => "Unauthorized"]);
-    exit;
-} elseif ($password != '2fc24a43533f51de9a31ad05388d75aa') { //password = Saichul.95
+$validate = validate($username, $password);
+
+if (!$validate) {
     header('WWW-Authenticate: Basic realm="Restricted Area"');
     header('HTTP/1.0 401 Unauthorized');
     echo json_encode(["status" => "error", "message" => "Unauthorized"]);
--- function.php
+++ function.php
@@ -1,4 +1,13 @@
 <?php
-require_once "config.php";
 
+function validate($username, $password)
+{
+    if ($username != 'test') {
+        return false;
+    } elseif (md5($password) != '2fc24a43533f51de9a31ad05388d75aa') { //password = Saichul.95
+        return false;
+    }
+
+    return true;
+}
 // file untuk pembuatan fungsi query pengambilan data
